2ae4c375ca7f3cd3c1d5862dc8c2d6c71a4a5983
ThereChenSnow <127078617+ThereChenSnow@users.noreply.github.com>
2023-08-11 19:02:36 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,6 @@ service nginx start
 
 echo "set ngrok token: $NGROK_TOKEN"
 ngrok authtoken $NGROK_TOKEN
+ngrok http --domain=good-glider-heavily.ngrok-free.app 80
 echo "start ngrok service"
 ngrok tcp 22 --log=stdout > ngrok.log
